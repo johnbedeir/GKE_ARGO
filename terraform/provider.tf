9dfef554908eb8b821db9c5b4fb3787b2a0556c5
John Bedeir <johnbedeir@Johns-MacBook-Pro.local>
2025-07-20 15:10:44 -0400

    update k8s and helm

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,14 +7,6 @@ terraform {
     random = {
       source = "hashicorp/random"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.0"
-    }
   }
 
   required_version = ">= 0.12"
@@ -34,7 +26,13 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  kubernetes = kubernetes
+  kubernetes {
+    host  = google_container_cluster.primary.endpoint
+    token = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(
+      google_container_cluster.primary.master_auth[0].cluster_ca_certificate,
+    )
+  }
 }
 
 data "google_client_config" "default" {}
