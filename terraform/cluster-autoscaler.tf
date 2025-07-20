4a58d0cbc833f1c2fe9001eb3096dd39a53851eb
John Bedeir <johnbedeir@Johns-MacBook-Pro.local>
2025-07-20 17:04:44 -0400

    1

--- terraform/cluster-autoscaler.tf
+++ terraform/cluster-autoscaler.tf
@@ -1,48 +1,48 @@
-resource "helm_release" "cluster-autoscaler" {
-  name       = "cluster-autoscaler"
-  namespace  = "kube-system" // It's common to deploy Cluster Autoscaler in the kube-system namespace
-  repository = "https://kubernetes.github.io/autoscaler"
-  chart      = "cluster-autoscaler"
-  version    = "9.36.0"
+# resource "helm_release" "cluster-autoscaler" {
+#   name       = "cluster-autoscaler"
+#   namespace  = "kube-system" // It's common to deploy Cluster Autoscaler in the kube-system namespace
+#   repository = "https://kubernetes.github.io/autoscaler"
+#   chart      = "cluster-autoscaler"
+#   version    = "9.36.0"
 
-  set {
-    name  = "cloudProvider"
-    value = "gce"
-  }
+#   set {
+#     name  = "cloudProvider"
+#     value = "gce"
+#   }
 
-  set {
-    name  = "rbac.create"
-    value = "true"
-  }
+#   set {
+#     name  = "rbac.create"
+#     value = "true"
+#   }
 
-  // GKE-specific settings
-  set {
-    name  = "autoDiscovery.clusterName"
-    value = "${var.name_prefix}-${var.environment}"
-  }
+#   // GKE-specific settings
+#   set {
+#     name  = "autoDiscovery.clusterName"
+#     value = "${var.name_prefix}-${var.environment}"
+#   }
 
-  set {
-    name  = "autoDiscovery.enabled"
-    value = "true"
-  }
+#   set {
+#     name  = "autoDiscovery.enabled"
+#     value = "true"
+#   }
 
-  set {
-    name  = "extraArgs.balance-similar-node-groups"
-    value = "true"
-  }
+#   set {
+#     name  = "extraArgs.balance-similar-node-groups"
+#     value = "true"
+#   }
 
-  set {
-    name  = "extraArgs.skip-nodes-with-system-pods"
-    value = "false"
-  }
+#   set {
+#     name  = "extraArgs.skip-nodes-with-system-pods"
+#     value = "false"
+#   }
 
-  set {
-    name  = "extraArgs.scale-down-enabled"
-    value = "true"
-  }
+#   set {
+#     name  = "extraArgs.scale-down-enabled"
+#     value = "true"
+#   }
 
-  set {
-    name  = "extraArgs.scale-down-unneeded-time"
-    value = "5m"
-  }
-}
+#   set {
+#     name  = "extraArgs.scale-down-unneeded-time"
+#     value = "5m"
+#   }
+# }
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,15 +7,23 @@ terraform {
     random = {
       source = "hashicorp/random"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.5"
+    }
   }
 
   required_version = ">= 0.12"
 }
 
 provider "google" {
-  credentials = file("gcp-credentials.json")
-  project     = var.project_id
-  region      = var.region
+  #credentials = file("gcp-credentials.json")
+  project = var.project_id
+  region  = var.region
 }
 
 provider "kubernetes" {
@@ -24,16 +32,6 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(
     google_container_cluster.primary.master_auth[0].cluster_ca_certificate,
   )
-}
-
-provider "helm" {
-  kubernetes {
-    host  = google_container_cluster.primary.endpoint
-    token = data.google_client_config.default.access_token
-    cluster_ca_certificate = base64decode(
-      google_container_cluster.primary.master_auth[0].cluster_ca_certificate,
-    )
-  }
 }
 
 data "google_client_config" "default" {}
