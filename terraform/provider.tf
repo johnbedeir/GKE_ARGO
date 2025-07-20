8da6c35481e26cc37104a0bf80096ed49e740346
John Bedeir <johnbedeir@Johns-MacBook-Pro.local>
2025-07-20 15:06:00 -0400

    add k8s and helm

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -25,4 +25,16 @@ provider "google" {
   region  = var.region
 }
 
+provider "kubernetes" {
+  host  = google_container_cluster.primary.endpoint
+  token = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(
+    google_container_cluster.primary.master_auth[0].cluster_ca_certificate,
+  )
+}
+
+provider "helm" {
+  kubernetes = kubernetes
+}
+
 data "google_client_config" "default" {}
